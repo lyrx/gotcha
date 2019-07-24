b11c8f70bb628090b10bb245fd656acdf94f0a9f
alex <enna@mailbox.org>
2019-07-24 17:46:41 +0200

    setup webpage with template

--- src/main/scala/com/lyrx/gotcha/Main.scala
+++ src/main/scala/com/lyrx/gotcha/Main.scala
@@ -1,18 +1,36 @@
 package com.lyrx.gotcha
 
+import com.lyrx.pyramids.Ipfs.IpfsSupport
+import com.lyrx.pyramids.stellarsdk.Timeout
+import com.lyrx.pyramids.{Config, Pyramid}
 import me.shadaj.slinky.web.ReactDOM
-import me.shadaj.slinky.web.html._ // imports tags, such as "h1"
+import me.shadaj.slinky.web.html._
+import org.scalajs.dom.document
 
-import org.scalajs.dom.{document}
+import scala.concurrent.{ExecutionContext, Future}
 
 object Main {
+
+  implicit val ec = ExecutionContext.global
+  implicit val timeout: Timeout = new Timeout(30)
+  implicit val isTest: Boolean = true
+
+
+  def initPyramid()(implicit executionContext: ExecutionContext) =
+    Config.createFuture()
+        .flatMap(
+          Pyramid(_)
+            .loadPharaohKey())
+
 
 
 
   def main(args:Array[String]):Unit = {
     val root = document.getElementById("root")
-    println("Jocha hei: "+root)
-    ReactDOM.render(h1("Hollahei!" ), root)
+    initPyramid().map(p=>
+      ReactDOM.render(h1(s"Hollahei:${p.config.frontendData.message}" ), root)
+    )
+
   }
 
 
