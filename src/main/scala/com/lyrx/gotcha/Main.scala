7dc64487884ce7db1d21691ee641fe67f608f97d
alex <enna@mailbox.org>
2019-07-24 15:24:08 +0200

    setup webpage with template

--- src/main/scala/com/lyrx/gotcha/Main.scala
+++ src/main/scala/com/lyrx/gotcha/Main.scala
@@ -5,20 +5,14 @@ import me.shadaj.slinky.web.html._ // imports tags, such as "h1"
 
 import org.scalajs.dom.{document}
 
-import scala.scalajs.js
 object Main {
 
 
 
   def main(args:Array[String]):Unit = {
-
-    val reactContainer = document.createElement("div")
-    document.body.appendChild(reactContainer)
-
-    ReactDOM.render(
-      h1("hello!"), // this is Scala's version of JSX!
-      reactContainer
-    )
+    val root = document.getElementById("root")
+    println("Jocha hei: "+root)
+    ReactDOM.render(h1("Hollahei!" ), root)
   }
 
 
