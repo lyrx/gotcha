5c31f2c624973806807d687788523abf80d358f3
alex <enna@mailbox.org>
2019-07-25 08:48:41 +0200

    setup webpage with template

--- build.sbt
+++ build.sbt
@@ -2,7 +2,6 @@ import org.scalajs.core.tools.linker.ModuleInitializer
 
 enablePlugins(ScalaJSPlugin, ScalaJSBundlerPlugin, ScalablyTypedPlugin)
 
-
 name := "gotcha"
 version := "0.0.1"
 scalaVersion := "2.12.8"
@@ -16,26 +15,29 @@ scalaJSModuleInitializers += ModuleInitializer
   .mainMethod(
     "com.lyrx.pyramids.frontend.Main"
     ,"init")
-*/
+ */
 
 scalaJSUseMainModuleInitializer := true
 
-
+scalacOptions += "-P:scalajs:sjsDefinedByDefault"
 
 libraryDependencies ++= Seq(
   "org.scala-js" %%% "scalajs-dom" % "0.9.7",
-  "me.shadaj" %%% "slinky-web" % "0.1.0",
+  "me.shadaj" %%% "slinky-core" % "0.6.2",
+  "me.shadaj" %%% "slinky-web" % "0.6.2",
+  "me.shadaj" %%% "slinky-native" % "0.6.2",
+  "me.shadaj" %%% "slinky-hot" % "0.6.2",
+  "me.shadaj" %%% "slinky-scalajsreact-interop" % "0.6.2",
   "default" %%% "humbletokenizer" % "0.0.3",
   ScalablyTyped.R.react,
   ScalablyTyped.R.`react-dom`
 )
 
-npmDependencies in Compile ++= Seq(
-  "react" -> "16.8",
-  "react-dom" -> "16.8"
+npmDependencies in Compile ++= Seq("react" -> "16.8", "react-dom" -> "16.8")
 
-
-)
+// optional, but recommended; enables the @react macro annotation API
+addCompilerPlugin(
+  "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full)
 
 testFrameworks += new TestFramework("utest.runner.Framework")
 
--- src/main/scala/com/lyrx/gotcha/Main.scala
+++ src/main/scala/com/lyrx/gotcha/Main.scala
@@ -1,38 +1,46 @@
 package com.lyrx.gotcha
 
-import com.lyrx.pyramids.Ipfs.IpfsSupport
 import com.lyrx.pyramids.stellarsdk.Timeout
 import com.lyrx.pyramids.{Config, Pyramid}
-import me.shadaj.slinky.web.ReactDOM
-import me.shadaj.slinky.web.html._
 import org.scalajs.dom.document
+import slinky.core._
+import slinky.core.annotations.react
+import slinky.core.facade.ReactElement
+import slinky.web.ReactDOM
+import slinky.web.html._
 
-import scala.concurrent.{ExecutionContext, Future}
-
-object Main {
+import scala.concurrent.ExecutionContext
+import scala.scalajs.js
+import js.Dynamic.{literal => l}
+import scala.scalajs.js.annotation.ScalaJSDefined
+@react class Pyramidal extends Component {
+  case class State(pyramidOpt: Option[Pyramid])
+  case class Props(name: String)
 
   implicit val ec = ExecutionContext.global
   implicit val timeout: Timeout = new Timeout(30)
   implicit val isTest: Boolean = true
 
-
   def initPyramid()(implicit executionContext: ExecutionContext) =
-    Config.createFuture()
-        .flatMap(
-          Pyramid(_)
-            .loadPharaohKey())
+    Config
+      .createFuture()
+      .flatMap(
+        Pyramid(_)
+          .loadPharaohKey())
 
+  override def initialState: State = State(None)
 
+  def msg() = state.pyramidOpt.map(_.config.frontendData.message).getOrElse("")
 
+  override def render(): ReactElement = h1(s"${msg()}")
 
-  def main(args:Array[String]):Unit = {
-    val root = document.getElementById("root")
-    initPyramid().map(p=>
-      ReactDOM.render(h1(s"Hollahei:${p.config.frontendData.message}" ), root)
-    )
+  override def componentDidMount(): Unit =
+    initPyramid()
+      .map(p => setState(State(Some(p))))
 
-  }
+}
 
-
-
+object Main {
+  def main(args: Array[String]): Unit =
+    ReactDOM.render(Pyramidal(name = ""), document.getElementById("root"))
 }
