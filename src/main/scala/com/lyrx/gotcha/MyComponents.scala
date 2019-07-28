f83706533c6bae488c2b9e33e998a07deb224028
alex <enna@mailbox.org>
2019-07-28 21:37:06 +0200

    styles for cards

--- src/main/scala/com/lyrx/gotcha/MyComponents.scala
+++ src/main/scala/com/lyrx/gotcha/MyComponents.scala
@@ -151,28 +151,6 @@ object MyComponents {
 
   }
 
-  def simpleCard(description: String,
-                 amount: String,
-                 currency: String): ReactElement =
-    div(className := "col-xl-3 col-md-6 mb-4")(
-      div(className := "card border-left-primary shadow h-100 py-2")(
-        div(className := "card-body")(
-          div(className := "row no-gutters align-items-center")(
-            div(className := "col mr-2")(
-              div(className := "text-xs font-weight-bold text-primary text-uppercase mb-1")(
-                s"${description}"),
-              /* div(className := "text-xs font-weight-bold text-primary text-uppercase mb-1")(
-                state.account), */
-              div(className := "h5 mb-0 font-weight-bold text-gray-800")(
-                s"${currency} ${amount}")
-            ),
-            div(className := "col-auto")(
-              i(className := "fas fa-calendar fa-2x text-gray-300")
-            )
-          )
-        )
-      )
-    )
 
 
 
--- src/main/scala/com/lyrx/gotcha/PharaohBalance.scala
+++ src/main/scala/com/lyrx/gotcha/PharaohBalance.scala
@@ -1,11 +1,11 @@
 package com.lyrx.gotcha
 
 import com.lyrx.gotcha.Main.ec
-import com.lyrx.gotcha.MyComponents.simpleCard
 import com.lyrx.pyramids.Pyramid
 import slinky.core.Component
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
+import slinky.web.html.{className, div, i, s}
 
 import scala.concurrent.Future
 
@@ -19,6 +19,30 @@ import scala.concurrent.Future
   case class State(amount: String)
 
   override def initialState: State = State( amount = "")
+
+
+  def simpleCard(description: String,
+                 amount: String,
+                 currency: String): ReactElement =
+    div(className := "col-xl-3 col-md-6 mb-4")(
+      div(className := "card shadow h-100 py-2")(
+        div(className := "card-body")(
+          div(className := "row no-gutters align-items-center")(
+            div(className := "col mr-2")(
+              div(className := "text-xs font-weight-bold text-primary text-uppercase mb-1")(
+                s"${description}"),
+              /* div(className := "text-xs font-weight-bold text-primary text-uppercase mb-1")(
+                state.account), */
+              div(className := "h5 mb-0 font-weight-bold text-gray-800")(
+                s"${currency} ${amount}")
+            ),
+            div(className := "col-auto")(
+              i(className := "fas fa-calendar fa-2x text-gray-300")
+            )
+          )
+        )
+      )
+    )
 
 
   def readBalance() = props
