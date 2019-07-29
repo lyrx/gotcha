080aced9edd037f4a860ece74cc09f01483828f7
alex <enna@mailbox.org>
2019-07-29 20:11:15 +0200

    credentials upload

--- src/main/scala/com/lyrx/gotcha/Credentials.scala
+++ src/main/scala/com/lyrx/gotcha/Credentials.scala
@@ -3,12 +3,45 @@ package com.lyrx.gotcha
 import com.lyrx.gotcha.Main.ec
 import com.lyrx.pyramids.Pyramid
 import org.scalajs.dom
-import slinky.core.{Component, StatelessComponent}
+import org.scalajs.dom.{Event, File}
+import org.scalajs.dom.raw.{Blob, EventTarget}
+import slinky.core.{Component, StatelessComponent, SyntheticEvent}
 import slinky.core.annotations.react
 import slinky.core.facade.{React, ReactElement}
 import slinky.web.html._
+import typings.jqueryLib.JQueryEventObject
 
 import scala.concurrent.Future
+import scala.scalajs.js
+import scala.scalajs.js.UndefOr
+
+object Typings{
+
+
+
+
+  @js.native
+  trait DataTransferTarget extends EventTarget {
+
+    val files:js.UndefOr[js.Array[Blob]] = js.native
+
+  }
+
+
+  @js.native
+  trait DataTransferEvent extends JQueryEventObject {
+
+    val dataTransfer:UndefOr[DataTransferTarget] = js.native
+
+    //val originalEvent:UndefOr[DataTransferEvent]=js.native
+
+  }
+
+
+
+
+}
+
 
 @react class Credentials extends StatelessComponent {
 
@@ -28,9 +61,7 @@ import scala.concurrent.Future
       readBalance()
   }
 
-  /*
-   <input id="wallet-upload" type="file" name="name" style="display: none;"/>
-   */
+
   override def render(): ReactElement =
     div(className := "card shadow mb-4")(
       div(className := "card-header py-3")(
@@ -44,7 +75,15 @@ import scala.concurrent.Future
             className := "my-fileselector",
             `type` := "file",
             ref := fileField,
-            onChange := (e => { e.target })
+            onChange := ((e) => {
+            e.target
+              .asInstanceOf[Typings.DataTransferTarget]
+                .files.map(
+              _.headOption.map(f=>f.asInstanceOf[File])
+                .map(f=>println(s"File: ${f.name}"))
+            )
+
+            })
           )
         ),
         div(
