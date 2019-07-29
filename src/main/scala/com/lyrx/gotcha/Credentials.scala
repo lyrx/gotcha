8a3d7f401623af626f1540c3819067df59bdffc6
alex <enna@mailbox.org>
2019-07-29 18:00:59 +0200

    Save credentials

--- src/main/scala/com/lyrx/gotcha/Credentials.scala
+++ src/main/scala/com/lyrx/gotcha/Credentials.scala
@@ -2,14 +2,18 @@ package com.lyrx.gotcha
 
 import com.lyrx.gotcha.Main.ec
 import com.lyrx.pyramids.Pyramid
+import org.scalajs.dom
 import slinky.core.{Component, StatelessComponent}
 import slinky.core.annotations.react
-import slinky.core.facade.ReactElement
+import slinky.core.facade.{React, ReactElement}
 import slinky.web.html._
 
 import scala.concurrent.Future
 
 @react class Credentials extends StatelessComponent {
+
+  val fileField = React.createRef[dom.html.Input]
+
   case class Props(
       pyramidOpt: Option[Pyramid],
   )
@@ -24,9 +28,21 @@ import scala.concurrent.Future
       readBalance()
   }
 
+  /*
+   <input id="wallet-upload" type="file" name="name" style="display: none;"/>
+   */
   override def render(): ReactElement = div()(
+    input(
+      className :="my-hidden" ,
+      `type`:="file",
+      ref:=fileField,
+      onChange:= (e=>{e.target})
+    ),
     div(
-      a(href := "#", className := "btn btn-light btn-icon-split my-btn")(
+      a(href := "#",
+        className := "btn btn-light btn-icon-split my-btn",
+        onClick:=(e=>props.pyramidOpt.map(_.saveKeys()))
+      )(
         span(className := "icon text-white-50")(
           i(className := "fas fa-save")
         ),
