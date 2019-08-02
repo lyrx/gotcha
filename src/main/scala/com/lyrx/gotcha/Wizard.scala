4912e022c90a29954462318a6910547c5e73e95b
alex <enna@mailbox.org>
2019-08-02 09:33:42 +0200

    Wizard

---
+++ src/main/scala/com/lyrx/gotcha/Wizard.scala
@@ -0,0 +1,37 @@
+package com.lyrx.gotcha
+
+import com.lyrx.gotcha.MyComponents.pageHeading
+import com.lyrx.pyramids.Pyramid
+import slinky.core.{Component, StatelessComponent}
+import slinky.core.annotations.react
+import slinky.core.facade.ReactElement
+import slinky.web.html._
+
+@react class Wizard extends Component {
+
+
+  case class Props(pyramidOpt: Option[Pyramid],title:String,frags:Seq[String])
+  case class State(pageOpt:Option[String])
+
+
+
+  def renderContents():ReactElement = if(state.pageOpt.isDefined)
+    iframe(src:=state.pageOpt.get)
+  else div()
+
+
+
+  def render(): ReactElement =
+    div(className := "container-fluid")(
+      h1()(props.title),
+      renderContents()
+    )
+
+
+
+  override def initialState(): State = State(pageOpt=None)
+
+
+}
+
+
