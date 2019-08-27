4d5728f9d89b93d2b1ece8c1ce49d7a3ae643558
alex <enna@mailbox.org>
2019-08-27 12:18:16 +0200

    orbitdb to Pyramid

--- src/main/scala/com/lyrx/pyramids/P2PData.scala
+++ src/main/scala/com/lyrx/pyramids/P2PData.scala
@@ -9,4 +9,15 @@ case class P2PData(
                     , dbOpt:Option[OrbitDB]
                   ){
   def withTID(id:Option[String])=this.copy(stellar=this.stellar.withTID(id))
+
+
+  def withIpfs(ipfs:Ipfs)=this.copy(ipfsOpt=Some(ipfs))
+
+
+  def withOrbit(db:OrbitDB)=this.copy(dbOpt=Some(db))
+
+
+
+
+
 }
