b3893f6935cdd96bcc313d9a00d51895bb50394d
alex <enna@mailbox.org>
2019-09-24 12:49:23 +0200

    indentation

--- src/main/scala/com/lyrx/pyramids/IpfsTrait.scala
+++ src/main/scala/com/lyrx/pyramids/IpfsTrait.scala
@@ -7,36 +7,31 @@ import typings.nodeLib.{Buffer, bufferMod}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.scalajs.js.typedarray.ArrayBuffer
 
-trait IpfsTrait{
+trait IpfsTrait {
 
-
-
-  def saveBufferToIpfs(b:nodeLib.Buffer)(
+  def saveBufferToIpfs(b: nodeLib.Buffer)(
       implicit ctx: ExecutionContext): FOption[String]
 
+  def bufferToIpfs(buffer: nodeLib.Buffer)(
+      implicit ctx: ExecutionContext): Future[Option[String]]
 
-
-
-
-
-  def bufferToIpfs(buffer: nodeLib.Buffer)(implicit ctx: ExecutionContext): Future[Option[String]]
-
-  def readIpfs(aHash: String)(implicit executionContext: ExecutionContext): Future[Option[Buffer]]
+  def readIpfs(aHash: String)(
+      implicit executionContext: ExecutionContext): Future[Option[Buffer]]
 
   def readIpfsString(aHash: String)(
       implicit executionContext: ExecutionContext): Future[Option[String]]
 
 
 
+
+
+
   def saveArrayBufferToIpfs(b: ArrayBuffer)(
-    implicit ctx: ExecutionContext): FOption[String]  =
+      implicit ctx: ExecutionContext): FOption[String] =
     saveBufferToIpfs(bufferMod.Buffer.from(b))
 
-
-
-
-
-  def saveStringToIpfs(s: String)(implicit ctx: ExecutionContext): FOption[String] =
+  def saveStringToIpfs(s: String)(
+      implicit ctx: ExecutionContext): FOption[String] =
     saveBufferToIpfs(
       bufferMod.Buffer.from(s)
     )
